3f89dfda402578ffcb9c594835889985d7b68fd7
Viv Richards <1014468+vivrichards600@users.noreply.github.com>
2021-05-06 16:43:29 +0100

    PLATUI-1143 - Updating test script to run specs

--- src/main/g8/run_tests.sh
+++ src/main/g8/run_tests.sh
@@ -4,4 +4,4 @@ ENV=\${1:-local}
 # Scalafmt checks have been separated from the test command to avoid OutOfMemoryError in Jenkins
 sbt scalafmtCheckAll scalafmtSbtCheck
 
-sbt -Denvironment=\$ENV "testOnly uk.gov.hmrc.test.api.*"
+sbt -Denvironment=\$ENV "testOnly uk.gov.hmrc.test.api.specs.*"
