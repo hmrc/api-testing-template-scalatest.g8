33d920ea992778ef85ca419819ea1052f3cae228
Viv Richards <1014468+vivrichards600@users.noreply.github.com>
2021-05-06 13:23:58 +0100

    PLATUI-1143 - Removing some dependencies which are not initially required

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -3,10 +3,7 @@ import sbt._
 object Dependencies {
 
   val test = Seq(
-    "org.scalatest"     %% "scalatest"              % "3.0.7"  % Test,
     "org.scalatest"     %% "scalatest"              % "3.2.0"  % Test,
-    "junit"              % "junit"                  % "4.12"   % Test,
-    "com.novocode"       % "junit-interface"        % "0.11"   % Test,
     "com.typesafe"       % "config"                 % "1.3.2"  % Test,
     "com.typesafe.play" %% "play-ahc-ws-standalone" % "2.1.2"  % Test,
     "org.slf4j"          % "slf4j-simple"           % "1.7.25" % Test
